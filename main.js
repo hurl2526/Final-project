593f6e9506604687428d6d8d02a17c627e487f34
Patrick <patrick.hurley@codeimmersives.com>
2020-04-28 15:46:50 -0400

    almost there

--- main.js
+++ main.js
@@ -8,18 +8,19 @@
 
         // objects array
         this.myBooks = [
-            {Title: 'The Hobbit', Author: 'J. R. R. Tolkien', Started: 'January', Pages: 125 , Rating: 6},
-            {Title: 'War and Peace', Author: 'J. R. R. Tolkien', Started: 'February', Pages: 56, Rating: 8},
-            {Title: 'The Great Gatsby', Author: 'J. R. R. Tolkien', Started: 'April', Pages: 210, Rating: 7 }
+            {Title: 'The Hobbit', Author: 'J. R. R. Tolkien', Started: 'January', Pages: 125},
+            {Title: 'War and Peace', Author: 'Leo Tolstoy', Started: 'February', Pages: 56},
+            {Title: 'The Great Gatsby', Author: 'F. Scott Fitzgerald', Started: 'April', Pages: 210 }
         ]
-        this.header = ['Title', 'Author', 'Started', 'Pages', 'Rating'];
+        this.header = ['Title', 'Author', 'Started', 'Pages'];
 
               // CREATE A TABLE.
             this.createTable = function () {
             const table = document.createElement('table');
             table.setAttribute('id', 'booksTable'); 
 
-            let tr = table.insertRow();               // CREATE A ROW (FOR HEADER).
+            // CREATE A ROW FOR HEADER.
+            let tr = table.insertRow();
             for (let h = 0; h < this.header.length; h++) {
                 let th = document.createElement('th');
                 th.innerHTML = this.header[h];
@@ -40,35 +41,35 @@
 
                 // *** CANCEL OPTION.
                 tr.appendChild(this.td);
-                let lblCancel = document.createElement('label');
-                lblCancel.innerHTML = '✖';
-                lblCancel.setAttribute('onclick', 'booksApp.Cancel(this)');
-                lblCancel.setAttribute('style', 'display:none;');
-                lblCancel.setAttribute('title', 'Cancel');
-                lblCancel.setAttribute('id', 'lbl' + i);
-                this.td.appendChild(lblCancel);
+                let cancel = document.createElement('label');
+                cancel.innerHTML = '✖';
+                cancel.setAttribute('onclick', 'booksApp.Cancel(this)');
+                cancel.setAttribute('style', 'display:none;');
+                cancel.setAttribute('title', 'Cancel');
+                cancel.setAttribute('id', 'cancel' + i);
+                this.td.appendChild(cancel);
 
                 // *** SAVE.
                 tr.appendChild(this.td);
-                let btSave = document.createElement('input');
+                let save = document.createElement('input');
 
-                btSave.setAttribute('type', 'button');      // SET ATTRIBUTES.
-                btSave.setAttribute('value', 'Save');
-                btSave.setAttribute('id', 'Save' + i);
-                btSave.setAttribute('style', 'display:none;');
-                btSave.setAttribute('onclick', 'booksApp.Save(this)');       // ADD THE BUTTON's 'onclick' EVENT.
-                this.td.appendChild(btSave);
+                save.setAttribute('type', 'button');      // SET ATTRIBUTES.
+                save.setAttribute('value', 'Save');
+                save.setAttribute('id', 'Save' + i);
+                save.setAttribute('style', 'display:none;');
+                save.setAttribute('onclick', 'booksApp.Save(this)');       // ADD THE BUTTON's 'onclick' EVENT.
+                this.td.appendChild(save);
 
                 // *** UPDATE.
                 tr.appendChild(this.td);
-                let btUpdate = document.createElement('input');
+                let update = document.createElement('input');
 
-                btUpdate.setAttribute('type', 'button');    // SET ATTRIBUTES.
-                btUpdate.setAttribute('value', 'Update');
-                btUpdate.setAttribute('id', 'Edit' + i);
-                btUpdate.setAttribute('style', 'background-color:#44CCEB;');
-                btUpdate.setAttribute('onclick', 'booksApp.Update(this)');   // ADD THE BUTTON's 'onclick' EVENT.
-                this.td.appendChild(btUpdate);
+                update.setAttribute('type', 'button');    // SET ATTRIBUTES.
+                update.setAttribute('value', 'Update');
+                update.setAttribute('id', 'Edit' + i);
+                update.setAttribute('style', 'background-color:#44CCEB;');
+                update.setAttribute('onclick', 'booksApp.Update(this)');   // ADD THE BUTTON's 'onclick' EVENT.
+                this.td.appendChild(update);
 
                 // *** DELETE.
                 this.td = document.createElement('th');
@@ -81,7 +82,8 @@
                 this.td.appendChild(btDelete);
             }
 
-                // ADD A ROW AT THE END WITH BLANK TEXTBOXES AND A DROPDOWN LIST (FOR NEW ENTRY).
+                // ADD A ROW AT THE END WITH BLANK TEXTBOXES
+                
 
                 tr = table.insertRow();           // CREATE THE LAST ROW.
 
@@ -109,30 +111,35 @@
                 let div = document.getElementById('container');
                 div.innerHTML = '';
                 div.appendChild(table);
-            };
+            }; //END OF CREATE TABLE FUNCTION
 
             // CREATE NEW FUNCTION. Ask for help with this!!!!
         this.CreateNew = function (button) {
+          console.log('booksTable');
             let activeRow = button.parentNode.parentNode.rowIndex;
             let tab = document.getElementById('booksTable').rows[activeRow];
             let obj = {};
 
-            // ADD NEW VALUE TO myBooks ARRAY.
+            // ADD NEW VALUE TO MYBOOKS ARRAY.
+
+            document.getElementById('booksTable').rows[activeRow]
             for (let i = 0; i < this.header.length; i++) {
                 let td = tab.getElementsByTagName("td")[i];
-                if (td.childNodes[0].getAttribute('type') == 'text') {      // CHECK IF ELEMENT IS A TEXTBOX
                     let txtVal = td.childNodes[0].value;
                     if (txtVal != '') {
-                        obj[this.header[i]] = txtVal.trim();
+                      obj[this.header[i]] = txtVal;
                     }
                     else {
-                        obj = '';
-                        alert('must fill our all fields');
-                        break;
+                      obj = '';
+                      alert('must fill out all fields');
+                      break;
+                      
                     }
-                }
-            }
-                this.myBooks.push(obj);             // PUSH DATA TO THE ARRAY.
+                  }
+                  if(obj!== ''){
+
+                    this.myBooks.push(obj);             // PUSH DATA TO THE ARRAY.
+                  }
                 this.createTable();                 // REFRESH THE TABLE.
         }
 
@@ -149,12 +156,12 @@
             let activeRow = button.parentNode.parentNode.rowIndex;
 
             // HIDE THE SAVE BUTTON.
-            let btSave = document.getElementById('Save' + (activeRow - 1));
-            btSave.setAttribute('style', 'display:none;');
+            let save = document.getElementById('Save' + (activeRow - 1));
+            save.setAttribute('style', 'display:none;');
 
             // SHOW THE UPDATE BUTTON AGAIN.
-            let btUpdate = document.getElementById('Edit' + (activeRow - 1));
-            btUpdate.setAttribute('style', 'display:block; margin:0 auto; background-color:#44CCEB;');
+            let update = document.getElementById('Edit' + (activeRow - 1));
+            update.setAttribute('style', 'display:block; margin:0 auto; background-color:#44CCEB;');
 
             let tab = document.getElementById('booksTable').rows[activeRow];
 
@@ -169,7 +176,7 @@
         this.Update = function (button) {
             let activeRow = button.parentNode.parentNode.rowIndex;
             let tab = document.getElementById('booksTable').rows[activeRow];
-            for (let i = 1; i < 5; i++) {
+            for (let i = 1; i < 4; i++) {
                 if (i == 3 || i === 4) {
                     let td = tab.getElementsByTagName("td")[i];
                     let ele = document.createElement('input');      // TEXTBOX.
@@ -180,11 +187,11 @@
                 }
             }
 
-            let lblCancel = document.getElementById('lbl' + (activeRow - 1));
-            lblCancel.setAttribute('style', 'cursor:pointer; display:block; width:20px; float:left; position: absolute;');
+            let cancel = document.getElementById('cancel' + (activeRow - 1));
+            cancel.setAttribute('style', 'cursor:pointer; display:block; width:20px; float:left; position: absolute;');
 
-            let btSave = document.getElementById('Save' + (activeRow - 1));
-            btSave.setAttribute('style', 'display:block; margin-left:30px; float:left; background-color:#2DBF64;');
+            let save = document.getElementById('Save' + (activeRow - 1));
+            save.setAttribute('style', 'display:block; margin-left:30px; float:left; background-color:#2DBF64;');
 
             // HIDE THIS BUTTON.
             button.setAttribute('style', 'display:none;');
@@ -200,13 +207,12 @@
 
         // SAVE DATA.
         this.Save = function (button) {
-            let activeRow = button.parentNode.parentNode.rowIndex;
-            let tab = document.getElementById('booksTable').rows[activeRow];
-
-            // UPDATE myBooks ARRAY WITH VALUES.
-            for (let i = 1; i < this.header.length; i++) {
-                let td = tab.getElementsByTagName("td")[i];
-                if (td.childNodes[0].getAttribute('type') == 'text') {  // CHECK IF ELEMENT IS A TEXTBOX
+            for (let i = 3; i < this.header.length; i++) {
+              let activeRow = button.parentNode.parentNode.rowIndex;
+              let tab = document.getElementById('booksTable').rows[activeRow];
+              let td = tab.getElementsByTagName("td")[i];
+              console.log(td.childNodes[0]);
+                if (td.childNodes[0].getAttribute('type') === 'text') {  // CHECK IF ELEMENT IS A TEXTBOX
                     this.myBooks[(activeRow - 1)][this.header[i]] = td.childNodes[0].value;      // SAVE THE VALUE.
                 }
             }
